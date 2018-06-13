e0941cd6399833b14e83298e00e4b18e01342e84
Vitor <vitorhora1@gmail.com>
2018-06-13 11:02:41 -0300

    Corrigir o logout do app.

--- PortalAdvogado/PortalAdvogado/Views/LogoutView.xaml.cs
+++ PortalAdvogado/PortalAdvogado/Views/LogoutView.xaml.cs
@@ -13,11 +13,17 @@ namespace PortalAdvogado.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LogoutView : ContentPage
 	{
-		public LogoutView ()
+		public LogoutView()
 		{
             InitializeComponent();
+            //Navigation.InsertPageBefore(new LoginView(), this);
+            //Navigation.PopAsync();
+            //(Application.Current.MainPage as MasterDetailPage).MasterBehavior = MasterBehavior.Popover;
+            //(Application.Current.MainPage as MasterDetailPage).IsPresented = false;
+            //App.Current.MainPage = new NavigationPage(new LoginView());
+            //Navigation.PushAsync(new LoginView());
             //Navigation.PopToRootAsync();
-            App.Current.MainPage = new LoginView();
+            Navigation.PushModalAsync(new LoginView());
         }
 	}
 }
