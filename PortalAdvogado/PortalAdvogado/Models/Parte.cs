4b6ac6636012bbe5a81cd8cd3fe444a4f53373b7
Vitor <vitorhora1@gmail.com>
2018-06-14 10:08:30 -0300

    Criação de novas entidades.

---
+++ PortalAdvogado/PortalAdvogado/Models/Parte.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortalAdvogado.Models
+{
+    public class Parte
+    {
+        public string nomeParte { get; set; }
+        public string tipoParte { get; set; }
+        public List<Advogado> listaAdvogados { get; set; }
+    }
+}
