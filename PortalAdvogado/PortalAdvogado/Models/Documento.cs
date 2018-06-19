1f1cc8e2aad4acade51afcfd7abf6e7f6c1a5052
Vitor <vitorhora1@gmail.com>
2018-06-19 12:54:08 -0300

    Criação da tela de documentos.

--- PortalAdvogado/PortalAdvogado/Models/Documento.cs
+++ PortalAdvogado/PortalAdvogado/Models/Documento.cs
@@ -6,7 +6,15 @@ namespace PortalAdvogado.Models
 {
     public class Documento
     {
-        public string descDocumento { get; set; }
+        public string tipoDocumento { get; set; }
+        public string nomeDocumento { get; set; }
         public byte[] conteudoDocumento { get; set; }
+        public string descDocumentoFormatado
+        {
+            get
+            {
+                return string.Format("({0}) {1}", tipoDocumento, nomeDocumento);
+            }
+        }
     }
 }
---
+++ PortalAdvogado/PortalAdvogado/Views/DocumentosView.xaml.cs
@@ -0,0 +1,59 @@
+﻿using PortalAdvogado.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PortalAdvogado.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DocumentosView : ContentPage
+	{
+        public List<ListaDocumentoMovimento> listaDocumentosMovimento = new List<ListaDocumentoMovimento>();
+        public ListaDocumentoMovimento listaAux;
+        private List<ListaDocumentoMovimento> _listaFinalDocumentosMovimento;
+        public List<ListaDocumentoMovimento> ListaFinalDocumentosMovimento { get { return _listaFinalDocumentosMovimento; } set { _listaFinalDocumentosMovimento = value; base.OnPropertyChanged(); } }
+
+        public DocumentosView (List<List<Movimento>> listaMovimentos)
+		{
+			InitializeComponent ();
+            if (listaMovimentos != null && listaMovimentos.Any())
+            {
+                foreach (var movimento in listaMovimentos[0])
+                {
+                    listaAux = new ListaDocumentoMovimento(movimento.listaDocumentos);
+                    listaAux.descMovimento = movimento.descMovimento;
+                    listaAux.dataMovimento = movimento.dataMovimento;
+                    this.listaDocumentosMovimento.Add(listaAux);
+                }
+                this.ListaFinalDocumentosMovimento = this.listaDocumentosMovimento;
+                DocumentosListView.ItemsSource = this.ListaFinalDocumentosMovimento;
+            }
+            else
+            {
+                EmptyListLabel.IsVisible = true;
+                DocumentosListView.IsVisible = false;
+            }
+        }
+
+        public void OnDocumentoClicked(object sender, EventArgs e)
+        {
+        }
+
+        public class ListaDocumentoMovimento : List<Documento>
+        {
+            public string descMovimento { get; set; }
+            public DateTime dataMovimento { get; set; }
+
+            public ListaDocumentoMovimento(List<Documento> documentos)
+            {
+                this.AddRange(documentos);
+            }
+        }
+
+    }
+}
